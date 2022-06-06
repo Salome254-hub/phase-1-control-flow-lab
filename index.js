f0ea6817d3fde3fc77dc72c64911d78353e2a32d
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-06 23:26:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+
+  if (scuberGreetingForFeet <= 400) {
+
+    console.log ('This one is on me!');
+  }
+
+  else if (scuberGreetingForFeet > 2000) {
+  
+    console.log ('I will gladly take your thirty bucks.');
+
+  }
+  else (scuberGreetingForFeet > 2500) ;{
+
+    console.log ('No can do.');
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){return (city === "NYC") ?  'Ok, sounds good.' : 'No go.';}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(string){
+  switch(string){
+  case 'generous':
+  return'Thank you so much.'
+   
+   case'not as generous':
+   return 'Thank you.'
+   
+   case'thanks for everything':
+   return 'Bye.'}
+  }
